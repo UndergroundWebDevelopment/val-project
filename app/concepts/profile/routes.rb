678c3ea3ea4fcb84dbf95c8043a7511b4ade615b
Alex Willemsma <alex@undergroundwebdevelopment.com>
2016-04-17 16:36:51 -0400

    Make profiles public by default, but create and update require auth.

--- app/concepts/profile/routes.rb
+++ app/concepts/profile/routes.rb
@@ -2,16 +2,17 @@ module ValProject
   class Profile
     class Routes < JsonAPIBaseRoute
       get "/profiles/:id" do
-        warden.authenticate!
         Update.present(params).to_json
       end
 
       post "/profiles" do
+        warden.authenticate!
         params[:profile] = request.body.read
         Create.(params).to_json
       end
 
       put "/profiles/:id" do
+        warden.authenticate!
         params[:profile] = request.body.read
         Update.(params).to_json
       end
